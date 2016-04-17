bc7f58dd29b1cc2b928e14df7b820241618c808d
goodluck-yh <goodluck.yh2014@gmail.com>
2016-04-17 12:10:16 +1000

    Delete all files

--- source/menu.php
+++
@@ -1,9 +0,0 @@
-<link href="css/menu.css" type="text/css" rel="stylesheet" />
-
-
-<div id="menu">
-    <a href="home.php">Home</a>&nbsp&nbsp
-    <a href="search.php">Search Flights</a>&nbsp&nbsp
-    <a href="yourbooking.php">Your bookings</a>&nbsp&nbsp
-    <a href="contact.php">Contact</a>
-</div>
